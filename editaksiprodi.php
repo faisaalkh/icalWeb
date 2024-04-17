dce3bfa87c2c89cab22dcf5230871c38f5c544b7
faisaalkh <165044013+faisaalkh@users.noreply.github.com>
2024-04-17 08:28:41 +0700

    Add files via upload

---
+++ editaksiprodi.php
@@ -0,0 +1,32 @@
+
+<?php
+require 'koneksi.php';
+
+$id = $_POST['id_prodi'];
+$prodi = $_POST['namaprodi'];
+
+//echo 'nama prodinya adalah: ' . $prodi ;
+
+//$query = "INSERT INTO prodi (Nama_Prodi) VALUES ('$prodi')";
+
+$query = "UPDATE prodi SET nama_prodi='$prodi' WHERE id_prodi =$id ";
+
+mysqli_query($conn, $query);
+
+if (mysqli_affected_rows($conn) > 0) {
+    echo "
+            <script>
+            alert('Data Berhasil Ditambahkan');
+            document.location.href='prodi.php';
+            </script>
+    ";
+} else {
+    echo "
+    <script>
+    alert ('Data gagal ditambahkan');
+    </script>
+";
+    echo mysqli_error($conn);
+}
+;
+?>
